1391ae9fefa2e0d350629875fda119bbd96f12aa
1912820899 <779931346@qq.com>
2022-06-18 16:30:31 +0800

    初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app">
+    <h1>抢占式开发工具</h1>
+    <div class="options">
+      <n-button type="primary"> 开启 </n-button>
+      <n-button type="error"> 关闭 </n-button>
+    </div>
+    <h2>当前状态</h2>
+    <n-progress
+      type="line"
+      :percentage="60"
+      :indicator-placement="'inside'"
+      processing
+    />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.app {
+  text-align: center;
+  .options {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
